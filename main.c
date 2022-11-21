1839427c2dd64530d6e2444d0274d8d2a99f08c2
Roy Bar-On <roy.bar@campus.technion.ac.il>
2022-11-21 17:05:56 +0200

    Implemented main function
    Also left declarations and stuff.

--- main.c
+++ main.c
@@ -1,12 +1,109 @@
 #include "AsciiArtTool.h"
 
+/* ---------- Macros ---------- */
 
-int main(int argc, char * argv[]) {
+#define CLOSE_FILE(ptr) do {    \
+    if (NULL != ptr)            \
+    {                           \
+        fclose(ptr);            \
+        ptr = NULL;             \
+    }                           \
+} while (0)
+
+
+/**
+ * Save the image in path `source` into a file in path `target` as an encoded
+ * ascii art.
+ *
+ * @param source: File handle to source ascii image.
+ * @param target: File handle to write encoded image into.
+ * */
+void encode_image(FILE * source, FILE * target)
+{
+
+}
+
+
+/**
+ * Save the image in path `source` into a file in path `target` as an inverted
+ * ascii art.
+ *
+ * @param source: File handle to source ascii image.
+ * @param target: File handle to write inverted image into.
+ * */
+void invert_image(FILE * source, FILE * target)
+{
+
+}
+
+
+void open_files(
+        char * source_path,
+        char * target_path,
+        FILE ** source_file,
+        FILE ** target_file)
+{
+    FILE * source_out = NULL;
+    FILE * target_out = NULL;
+
+    source_out = fopen(source_path, "rt");
+    if (NULL == source_out)
+    {
+        *source_file = NULL;
+        *target_file = NULL;
+        return;
+    }
+
+    target_out = fopen(target_path, "wt");
+    if (NULL == target_out)
+    {
+        CLOSE_FILE(source_out);
+        *target_file = NULL;
+        return;
+    }
+
+    *source_file = source_out;
+    *target_file = target_out;
+}
+
+
+int main(int argc, char * argv[])
+{
+    char * mode = NULL;
+    FILE * source_file = NULL;
+    FILE * target_file = NULL;
+
     if (4 != argc)
     {
         /* TODO: Remove usage/find required print. */
         printf("USAGE: asciiArtTool FLAG SOURCE TARGET\n");
         return 0;
     }
+    mode = argv[1];
+
+    open_files(argv[2], argv[3], &source_file, &target_file);
+    if (NULL == source_file || NULL == target_file)
+    {
+        CLOSE_FILE(source_file);
+        CLOSE_FILE(target_file);
+    }
+
+    if (mode[0] != '-')
+    {
+        /* TODO: Error message? Non-zero output? */
+        return 0;
+    }
+
+
+    switch(mode[1])
+    {
+        case 'e':
+            encode_image(source_file, target_file);
+            break;
+        case 'i':
+            invert_image(source_file, target_file);
+            break;
+    }
+
     return 0;
 }
