97be0a212b9642890b69d40f4f182bc2a2ed9aff
Roy Bar-On <roy.bar@campus.technion.ac.il>
2022-11-21 17:05:27 +0200

    Added main for project

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "AsciiArtTool.h"
+
+
+int main(int argc, char * argv[]) {
+    if (4 != argc)
+    {
+        /* TODO: Remove usage/find required print. */
+        printf("USAGE: asciiArtTool FLAG SOURCE TARGET\n");
+        return 0;
+    }
+    return 0;
+}
